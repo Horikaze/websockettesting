ac0ae0d1800e041407b72d083dedde524d7d0c38
Horikaze <adrianyt659@gmail.com>
2023-08-13 14:50:40 +0200

    meow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,32 +3,33 @@ import { Toaster, toast } from "react-hot-toast";
 import io from "socket.io-client";
 const socket = io("http://localhost:3001");
 function App() {
-  const [message, setMessage] = useState("");
-  const [recived, setRecived] = useState("");
+  const [range, setRange] = useState(0);
   const [room, setRoom] = useState("");
-  const sendMessage = () => {
+
+  const sendMessage = (message: number) => {
     socket.emit("send_message", {
       message,
       room,
     });
+    setRange(message);
   };
   const joinRoom = () => {
     if (room != "") {
       socket.emit("join_room", room);
-      toast.success(`Dołączono do: ${room}`)
+      toast.success(`Dołączono do: ${room}`);
     }
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      setRecived(data.message);
+      setRange(data.message);
     });
   }, []);
   return (
     <div className="flex flex-col">
-      <div><Toaster
-      position="top-right"
-      /></div>
+      <div>
+        <Toaster position="top-right" />
+      </div>
       <div className="flex justify-center">
         <div className="flex flex-row gap-2">
           <input
@@ -45,8 +46,16 @@ function App() {
           >
             Join
           </button>
+          <input
+            type="range"
+            value={range}
+            onChange={(e) => {
+              sendMessage(Number(e.target.value));
+            }}
+          />
         </div>
       </div>
+      <p>{`Msg:${range}`}</p>
     </div>
   );
 }
